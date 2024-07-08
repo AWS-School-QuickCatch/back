411e12dd48adc27b5fb76e264ddcb63da55df8d3
danma <dong5053@naver.com>
2024-07-09 00:09:22 +0900

    for ingress healthcheck final

--- myapp/urls.py
+++ myapp/urls.py
@@ -4,7 +4,7 @@ from .views import health_check
 
 
 urlpatterns = [
-    path('api', health_check),
+    path('', health_check),
     path('api/live/mainlist', views.BroadcastProductListView.as_view(), name='broadcast-product-list'),   #홈쇼핑 방송상품 리스트 메인 페이지(홈쇼핑사별 호출)
     path('api/live/details', views.BroadcastProductDetails.as_view(), name='broadcast-product-details'),  #홈쇼핑 방송상품 상세 페이지
     path('api/compare/details', views.SimilarProductList.as_view(), name='similar-product-list'),         #유사상품 리스트 호출
